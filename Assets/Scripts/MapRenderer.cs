af5a21f393f06980648d32a2b7d9e7a55fd06273
mwehrstedt <marcel.wehrstedt@outlook.com>
2015-05-16 14:35:58 +0200

    hopefully uploading assets and script

---
+++ Assets/Scripts/MapRenderer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapRenderer : MonoBehaviour
+{
+
+    enum MapElement
+    {
+        Floor,
+        Wall,
+        Snake,
+        Cherry,
+        Apple
+    }
+
+    public GameObject floorSprite;
+
+    MapElement[,] Map;
+    int WidthTiles;
+    int HeightTiles;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        Map = new MapElement[Screen.width / 64, Screen.height / 64];
+        for (int x = 0; x < Map.GetLength(0); x++)
+        {
+            for (int y = 0; y < Map.GetLength(1); y++)
+            {
+                Map[x, y] = MapElement.Floor;
+            }
+        }
+
+        // Initialize map
+        for (int x = 0; x < Map.GetLength(0); x++)
+        {
+            for (int y = 0; y < Map.GetLength(1); y++)
+            {
+                GameObject temp = Instantiate(floorSprite) as GameObject;
+                temp.transform.position = new Vector3(x * 32, y * 32);
+                Debug.Log("position: " + x.ToString() + ", " + y.ToString());
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SnakeController : MonoBehaviour {
+		
+	Vector2 position;
+	List<SnakeElement> bodyParts;
+	float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
