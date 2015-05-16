3a4f7de4d96f96e98e038501a6e82f0dbe20a36e
YsenGrimm <kloeten.wars@googlemail.com>
2015-05-16 16:16:27 +0200

    MapRenderer + Camera movement :snake:

--- Assets/Scripts/MapRenderer.cs
+++ Assets/Scripts/MapRenderer.cs
@@ -5,50 +5,105 @@ using System.Collections.Generic;
 public class MapRenderer : MonoBehaviour
 {
 
-    enum MapElement
-    {
-        Floor,
-        Wall,
-        Snake,
-        Cherry,
-        Apple
-    }
+    public GameObject FloorSprite;
+	public GameObject WallSprite;
+	public GameObject MapParent;
 
-    public GameObject floorSprite;
-
-    MapElement[,] Map;
+    int[,] Map;
     int WidthTiles;
     int HeightTiles;
 
     // Use this for initialization
     void Start()
     {
+		WidthTiles = 16;//(int)Mathf.Ceil(Screen.width / 64.0f);
+		HeightTiles = 12;//(int)Mathf.Ceil(Screen.height / 64.0f);
         
-        Map = new MapElement[Screen.width / 64, Screen.height / 64];
-        for (int x = 0; x < Map.GetLength(0); x++)
-        {
-            for (int y = 0; y < Map.GetLength(1); y++)
-            {
-                Map[x, y] = MapElement.Floor;
-            }
-        }
-
-        // Initialize map
-        for (int x = 0; x < Map.GetLength(0); x++)
-        {
-            for (int y = 0; y < Map.GetLength(1); y++)
-            {
-                GameObject temp = Instantiate(floorSprite) as GameObject;
-                temp.transform.position = new Vector3(x * 32, y * 32);
-                Debug.Log("position: " + x.ToString() + ", " + y.ToString());
-            }
-        }
+		Map = new int[,] {
+			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1},
+			{1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+		};
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		if (Input.GetKeyUp(KeyCode.W)) {
+			Camera.main.gameObject.transform.position = new Vector3(Camera.main.gameObject.transform.position.x, 
+			                                                        Camera.main.gameObject.transform.position.y + 12, 
+			                                                        Camera.main.gameObject.transform.position.z);
+		}
+		if (Input.GetKeyUp(KeyCode.A)) {
+			Camera.main.gameObject.transform.position = new Vector3(Camera.main.gameObject.transform.position.x - 24, 
+			                                                        Camera.main.gameObject.transform.position.y, 
+			                                                        Camera.main.gameObject.transform.position.z);
+		}
+		if (Input.GetKeyUp(KeyCode.S)) {
+			Camera.main.gameObject.transform.position = new Vector3(Camera.main.gameObject.transform.position.x, 
+			                                                        Camera.main.gameObject.transform.position.y - 12, 
+			                                                        Camera.main.gameObject.transform.position.z);
+		}
+		if (Input.GetKeyUp(KeyCode.D)) {
+			Camera.main.gameObject.transform.position = new Vector3(Camera.main.gameObject.transform.position.x + 24, 
+			                                                        Camera.main.gameObject.transform.position.y, 
+			                                                        Camera.main.gameObject.transform.position.z);
+		}
+		
+		for (int child = 0; child < MapParent.transform.childCount; child++) {
+			GameObject.Destroy(MapParent.transform.GetChild(child).gameObject);
+		}
+
+		for (int y = 0; y < Map.GetLength(0); y++)
+		{
+			for (int x = 0; x < Map.GetLength(1); x++)
+			{
+				GameObject NewMapElement ;
+				switch (Map[y,x]) {
+				case 0:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+				case 1:
+					NewMapElement = Instantiate(WallSprite) as GameObject;
+					break;
+				case 2:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+				case 3:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+				case 4:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+				default:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+				}
+				NewMapElement.transform.position = new Vector3(x - (WidthTiles/2.0f), y - (HeightTiles/2.0f), 0);
+				NewMapElement.transform.parent = MapParent.transform;
+			}
+		}
     }
 }
