0f10bec28a1a17ed5db5cf784815142291c9baff
YsenGrimm <kloeten.wars@googlemail.com>
2015-05-17 13:21:07 +0200

    fixes #10 #14 #15

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit {
+
+	public enum FruitType {
+		Cherry,
+		Apple,
+
+		length
+	}
+
+	public Vector2 fruitPos;
+	FruitType type;
+
+	float livetime;
+	float decaySpeed;
+
+	bool alive;
+
+	public int FruitTypeInt;
+
+	public Fruit(FruitType type, Vector2 pos, float decay) {
+		this.fruitPos = pos;
+		this.type = type;
+		this.alive = true;
+
+		this.decaySpeed = decay;
+
+		switch (this.type) {
+		case Fruit.FruitType.Cherry:
+			this.livetime = 10;
+			this.FruitTypeInt = 4;
+			break;
+		case Fruit.FruitType.Apple:
+			this.livetime = 20;
+			this.FruitTypeInt = 5;
+			break;
+		default:
+			this.livetime = 10;
+			break;
+		}
+	}
+
+	public void UpdateTimers() {
+		if (livetime < 0) {
+			alive = false;
+		}
+		if (alive) {
+			livetime -= decaySpeed;
+		}
+	}
+
+	public bool Alive() {
+		return this.alive;
+	}
+}
--- Assets/Scripts/MapRenderer.cs
+++ Assets/Scripts/MapRenderer.cs
@@ -9,6 +9,7 @@ public class MapRenderer : MonoBehaviour
     public GameObject FloorSprite;
     public GameObject WallSprite;
     public GameObject MapParent;
+	public GameObject MapParentStatic;
     public GameObject CherrySprite;
     public GameObject AppleSprite;
     public GameObject SnakeBody;
@@ -16,6 +17,16 @@ public class MapRenderer : MonoBehaviour
 
     public GameObject Score;
     public GameObject gameOverMenu;
+
+	public int FruitsInMap = 10;
+
+	public float CameraFollowDistance;
+	public float CameraNearDistance;
+	public AnimationCurve CameraCurve;
+
+	float cameraSteps = 0.01f;
+	float evalCameraPathAt = 0.0f;
+	bool moveCamera = false;
 
     int widthInTiles = 42;
     int heightInTiles = 24;
@@ -26,13 +37,15 @@ public class MapRenderer : MonoBehaviour
     bool menuShown = false;
     List<SnakeElement> bodyParts;
 
+	List<Fruit> FruitList;
+	int FruitCounter;
+
     // Use this for initialization
     void Start()
     {
-        Rooms rooms = new Rooms();
-
+		// Map generation
         Map = new int[heightInTiles, widthInTiles];
-
+		Rooms rooms = new Rooms();
         for (int x = 0; x < rooms.MapRoomsTopL[0].GetLength(1); x++)
         {
             for (int y = 0; y < rooms.MapRoomsTopL[0].GetLength(0); y++)
@@ -44,6 +57,47 @@ public class MapRenderer : MonoBehaviour
             }
         }
 
+		// Random Fruits
+		Random.seed = 1337;
+
+		FruitList = new List<Fruit>() {};
+
+		for (int i = 0; i < FruitsInMap; i++) {
+			Vector2 fruitPos;
+			do {
+				fruitPos = new Vector2(Random.value*widthInTiles, Random.value*heightInTiles);
+			} while (Map[(int)fruitPos.y, (int)fruitPos.x] == 1);
+			FruitList.Add(new Fruit(randomFruitType(Random.value), fruitPos, Random.value * 0.01f));
+			FruitCounter++;
+		}
+
+		for (int x = 0; x < Map.GetLength(1); x++)
+		{
+			for (int y = 0; y < Map.GetLength(0); y++)
+			{
+				GameObject NewMapElement;
+				
+				switch (Map[y, x])
+				{
+					// Ground
+				case 0:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+					// Wall
+				case 1:
+					NewMapElement = Instantiate(WallSprite) as GameObject;
+					break;
+					// Snake Head
+				default:
+					NewMapElement = Instantiate(FloorSprite) as GameObject;
+					break;
+				}
+				
+				NewMapElement.transform.position = new Vector3(x, -y, -1);
+				NewMapElement.transform.parent = MapParentStatic.transform;
+			}
+		}
+
         bodyParts = FindObjectOfType<SnakeController>().bodyParts;
     }
 
@@ -54,10 +108,31 @@ public class MapRenderer : MonoBehaviour
         {
             bodyParts = FindObjectOfType<SnakeController>().bodyParts;
         }
-
-        //SnakeController.SnakeDirections moveDirection = FindObjectOfType<SnakeController>().getMoveDirection();
+	
         if (!Dead)
         {
+
+			//Decay Fruits
+			if (FruitList.Count > 0) {
+				foreach (var fruit in new List<Fruit>(FruitList)) {
+					fruit.UpdateTimers();
+					if (!fruit.Alive()) {
+						Map[(int)fruit.fruitPos.y, (int)fruit.fruitPos.x] = 0;
+						FruitList.Remove(fruit);
+					}
+				}	
+			}
+
+			//spawn new fruits
+			if (FruitCounter < FruitsInMap) {
+				Vector2 fruitPos;
+				do {
+					fruitPos = new Vector2(Random.value * widthInTiles, Random.value * heightInTiles);
+				} while ((Map[(int)fruitPos.y, (int)fruitPos.x] == 1) && (Map[(int)fruitPos.y, (int)fruitPos.x] == 2) 
+				         &&  (Map[(int)fruitPos.y, (int)fruitPos.x] == 3));
+				FruitList.Add(new Fruit(randomFruitType(Random.value), fruitPos, Random.value * 0.01f));
+				FruitCounter++;
+			}
 
             // Collision importand before replace ;)
             if (Map[(int)bodyParts[0].MapPosition.y, (int)bodyParts[0].MapPosition.x] == 1)
@@ -69,26 +144,36 @@ public class MapRenderer : MonoBehaviour
             if (Map[(int)bodyParts[0].MapPosition.y, (int)bodyParts[0].MapPosition.x] == 4)
             {
                 NewSnakeElements += 5;
+				FruitCounter--;
+				foreach (var fruit in new List<Fruit>(FruitList)) {
+					if (new Vector3(fruit.fruitPos.x, fruit.fruitPos.y, bodyParts[0].MapPosition.z) == bodyParts[0].MapPosition) {
+						FruitList.Remove(fruit);
+					}
+				}
             }
 
             //collision with apple
             if (Map[(int)bodyParts[0].MapPosition.y, (int)bodyParts[0].MapPosition.x] == 5)
             {
                 NewSnakeElements += 15;
+				FruitCounter--;
+				foreach (var fruit in new List<Fruit>(FruitList)) {
+					if (new Vector3(fruit.fruitPos.x, fruit.fruitPos.y, bodyParts[0].MapPosition.z) == bodyParts[0].MapPosition) {
+						FruitList.Remove(fruit);
+					}
+				}
             }
 
+			foreach (var fruit in FruitList) {
+				Map[(int)fruit.fruitPos.y, (int)fruit.fruitPos.x] = fruit.FruitTypeInt;
+			}
+
+			// add new snake elements when eaten
             while (NewSnakeElements > 0)
             {
                 bodyParts.Add(new SnakeElement(new Vector3(bodyParts[bodyParts.Count - 1].MapPosition.x, bodyParts[bodyParts.Count - 1].MapPosition.y, -2)));
                 NewSnakeElements--;
             }
-
-            // commented because of menu
-            // reset level on death
-            //if (Dead)
-            //{
-            //    Application.LoadLevel("Home");
-            //}
 
             // draw snake only when not dead because better looks
             if (!Dead)
@@ -101,14 +186,31 @@ public class MapRenderer : MonoBehaviour
                 }
             }
 
+			// self collision
             if (Map[(int)bodyParts[0].MapPosition.y, (int)bodyParts[0].MapPosition.x] == 3)
             {
                 Dead = true;
             }
 
             // attach camera to snake head
-            Camera.main.gameObject.transform.position = new Vector3(bodyParts[0].MapPosition.x, -bodyParts[0].MapPosition.y, -10);
+			Vector2 oldCameraPos = new Vector2(Camera.main.transform.position.x, -Camera.main.transform.position.y);
+			Vector2 playerPos = bodyParts[0].MapPosition;
 
+			if (Vector3.Distance(oldCameraPos, playerPos) > CameraFollowDistance) {
+				moveCamera = true;
+			} else if (Vector3.Distance(oldCameraPos, playerPos) < CameraNearDistance) {
+				moveCamera = false;
+				evalCameraPathAt = 0.0f;
+			}
+
+			if (moveCamera) {
+				evalCameraPathAt += cameraSteps;
+				Vector2 newCameraPos = Vector2.Lerp(oldCameraPos, playerPos, CameraCurve.Evaluate(evalCameraPathAt));
+				Camera.main.gameObject.transform.position = new Vector3(newCameraPos.x, -newCameraPos.y, Camera.main.transform.position.z);
+			}
+			print(oldCameraPos + "  " + bodyParts[0].MapPosition);
+
+			// cleanup draw list
             for (int child = 0; child < MapParent.transform.childCount; child++)
             {
                 GameObject.Destroy(MapParent.transform.GetChild(child).gameObject);
@@ -118,58 +220,37 @@ public class MapRenderer : MonoBehaviour
             {
                 for (int y = 0; y < Map.GetLength(0); y++)
                 {
-                    GameObject NewMapElement;
-                    GameObject FloorMapElement;
-
                     switch (Map[y, x])
                     {
-                        // Ground
-                        case 0:
-                            NewMapElement = Instantiate(FloorSprite) as GameObject;
-                            break;
-                        // Wall
-                        case 1:
-                            NewMapElement = Instantiate(WallSprite) as GameObject;
-                            break;
                         // Snake Head
                         case 2:
-                            NewMapElement = Instantiate(SnakeHead) as GameObject;
-
-                            FloorMapElement = Instantiate(FloorSprite) as GameObject;
-                            FloorMapElement.transform.position = new Vector3(x, -y, 0);
-                            FloorMapElement.transform.parent = MapParent.transform;
+							GameObject NewSnakeHeadElement;
+                            NewSnakeHeadElement = Instantiate(SnakeHead) as GameObject;
+							NewSnakeHeadElement.transform.position = new Vector3(x, -y, -1);
+							NewSnakeHeadElement.transform.parent = MapParent.transform;
                             break;
                         // Snake Body
                         case 3:
-                            NewMapElement = Instantiate(SnakeBody) as GameObject;
-
-                            FloorMapElement = Instantiate(FloorSprite) as GameObject;
-                            FloorMapElement.transform.position = new Vector3(x, -y, 0);
-                            FloorMapElement.transform.parent = MapParent.transform;
+							GameObject NewSnakeBodyElement;
+                            NewSnakeBodyElement = Instantiate(SnakeBody) as GameObject;
+							NewSnakeBodyElement.transform.position = new Vector3(x, -y, -1);
+							NewSnakeBodyElement.transform.parent = MapParent.transform;
                             break;
                         // Cherry
                         case 4:
-                            NewMapElement = Instantiate(CherrySprite) as GameObject;
-
-                            FloorMapElement = Instantiate(FloorSprite) as GameObject;
-                            FloorMapElement.transform.position = new Vector3(x, -y, 0);
-                            FloorMapElement.transform.parent = MapParent.transform;
+							GameObject NewCherryElement;
+                            NewCherryElement = Instantiate(CherrySprite) as GameObject;
+							NewCherryElement.transform.position = new Vector3(x, -y, -1);
+							NewCherryElement.transform.parent = MapParent.transform;
                             break;
                         // Apple
                         case 5:
-                            NewMapElement = Instantiate(AppleSprite) as GameObject;
-
-                            FloorMapElement = Instantiate(FloorSprite) as GameObject;
-                            FloorMapElement.transform.position = new Vector3(x, -y, 0);
-                            FloorMapElement.transform.parent = MapParent.transform;
-                            break;
-                        default:
-                            NewMapElement = Instantiate(FloorSprite) as GameObject;
+							GameObject NewAppleElement;
+                            NewAppleElement = Instantiate(AppleSprite) as GameObject;
+							NewAppleElement.transform.position = new Vector3(x, -y, -1);
+							NewAppleElement.transform.parent = MapParent.transform;
                             break;
                     }
-
-                    NewMapElement.transform.position = new Vector3(x, -y, -1);
-                    NewMapElement.transform.parent = MapParent.transform;
 
                     if (Map[y, x] == 2 || Map[y, x] == 3)
                     {
@@ -190,4 +271,15 @@ public class MapRenderer : MonoBehaviour
             }
         }
     }
+
+	private Fruit.FruitType randomFruitType(float randNum) {
+		float randScaledToEnum = randNum * (int)Fruit.FruitType.length;
+		if (randScaledToEnum >= 0 && randScaledToEnum < 1) {
+			return Fruit.FruitType.Cherry;
+		} else if (randScaledToEnum > 1) {
+			return Fruit.FruitType.Apple;
+		} else {
+			return Fruit.FruitType.Apple;
+		}
+	}
 }
