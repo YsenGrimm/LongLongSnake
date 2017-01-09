f4622c5bb6f5180905c5b8c88cffb70548ec2a3a
YsenGrimm <kloeten.wars@googlemail.com>
2017-01-09 23:38:29 +0100

    keyboard menu

---
+++ Assets/Scripts/Menu/ButtonStyler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonStyler : MonoBehaviour {
+
+	private Image button;
+	private Text text;
+
+	public void Awake() {
+		button = GetComponent<Image>();
+		text = GetComponentInChildren<Text>();
+	}
+	
+	public void NotActive() {
+		button.color = new Color(1, 1, 1, 0.5f);
+		text.color = new Color(0, 0, 0, 0.5f);
+	}
+
+	public void Active() {
+		button.color = new Color(1, 1, 1, 1);
+		text.color = new Color(0, 0, 0, 1);
+	}
+}
--- Assets/Scripts/Menu/MenuBehaviourScript.cs
+++ Assets/Scripts/Menu/MenuBehaviourScript.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuBehaviourScript : MonoBehaviour {
+public class MenuBehaviourScript : MonoBehaviour 
+{
 
-    public void StartGame()
-    {
+    public ButtonStyler[] AnimatedButtons;
+
+    private int activeButton = 0;
+    private int buttonCount = 2;
+    private bool buttonUpDown;
+    private bool buttonDownDown;
+
+    public void Start() {
+        setButtonUpdate(0);
+    }
+
+    public void Update() {
+        // reset on "button up"
+        if (Input.GetAxisRaw("Vertical") >= 0 && buttonDownDown) {
+            buttonDownDown = false;
+        }
+
+        if (Input.GetAxisRaw("Vertical") <= 0 && buttonUpDown) {
+            buttonUpDown = false;
+        }
+
+        // up
+        if (Input.GetAxisRaw("Vertical") > 0 && !buttonUpDown)
+        {
+            activeButton = (activeButton + 1) > buttonCount - 1 ? 0 : activeButton + 1;
+            setButtonUpdate(activeButton);
+            buttonUpDown = true;
+        }
+
+        // down
+        if (Input.GetAxisRaw("Vertical") < 0 && !buttonDownDown)
+        {
+            activeButton = (activeButton - 1) < 0 ? buttonCount - 1 : activeButton - 1;
+            setButtonUpdate(activeButton);
+            buttonDownDown = true;
+        }
+
+        if (Input.GetButtonDown("Submit")) {
+            switch (activeButton)
+            {
+                case 0:
+                    StartGame();
+                    break;
+                case 1:
+                    QuitGame();
+                    break;
+            }
+        }
+    }
+
+    void setButtonUpdate(int buttonIdx) {
+        foreach (var button in AnimatedButtons) {
+            button.NotActive();
+        }
+
+        AnimatedButtons[buttonIdx].Active();
+    }
+
+    public void StartGame() {
 		SceneManager.LoadScene("Game");
     }
 
-    public void QuitGame()
-    {
+    public void QuitGame() {
         Application.Quit();
     }
 }
